28c1864216aa79a2ddcb988a19ff99cf0dc53161
Michel Hathier <mhathiergmail.com>
2021-10-07 09:48:28 +0200

    Update admin index posts page & admin dashboard page

--- html_css/js/main.js
+++ html_css/js/main.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-    $('.nav-btn').click(function(){
-        $('.responsive-nav').toggleClass('active');
-    });
-});
+const navbtn = document.querySelector(".nav-btn");
+const sidemenu = document.querySelector(".sidemenu");
+
+navbtn.addEventListener("click", () => {
+    navbtn.classList.toggle("active");
+    sidemenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    navbtn.classList.remove("active");
+    sidemenu.classList.remove("active");
+}))
 
 
 
