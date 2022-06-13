f314c85b705a32bcb4d7b77350bfa3ca7d72973c
Michel Hathier <mhathiergmail.com>
2022-06-13 13:56:22 +0200

    Correcting error security Codacy

--- controller/backEndController/commentsController.php
+++ controller/backEndController/commentsController.php
@@ -21,7 +21,7 @@ class commentsController extends baseController
         if($this->issetSession('update') && $this->getSession('update') != "") { ?>
             <script>
                 Swal.fire({
-                    title: "<?= print_r($this->getSession('update')); ?>",
+                    title: "<?php print_r($this->getSession('update')); ?>",
                     icon: 'success',
                     confirmButtonColor: '#1aBC9C',
                 })
